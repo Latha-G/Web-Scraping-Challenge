efaef06cd91cf39950c0cb0c3d01325cd5101e97
Latha-G <60633643+Latha-G@users.noreply.github.com>
2020-05-12 22:58:16 -0700

    a

---
+++ Mission To Mars/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect
+import scrape_mars
+
+# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# set inline
+mongo = PyMongo(app, uri="mongodb://localhost:27017/MARS_app")
+
+@app.route("/")
+def index():
+    
+    # Find one record of data from the mongo database
+    MARS_data = mongo.db.collection.find_one()
+
+    return render_template("index.html", MARS_data = MARS_data)
+
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    MARS_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.replace_one({}, MARS_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Mission To Mars/scrape_mars.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import requests
+
+import time
+import re
+
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+
+def init_browser():
+    
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def mars_weather():
+    
+    browser = init_browser()
+
+    # Url of Mars Weather twitter account
+    weather_url = 'https://twitter.com/marswxreport?lang=en'
+
+    browser.visit(weather_url)
+    time.sleep(5)
+    
+    weather_html = browser.html
+
+    weather_soup = BeautifulSoup(weather_html, 'lxml')
+
+    pattern = re.compile(r'InSight sol')
+    
+    try:
+        
+        mars_weather = weather_soup.find('span', text = pattern).text
+        
+    except:
+        
+        mars_weather = 'Some error occured while retrieving Mars weather data from Twitter. Please try after sometime.'
+        
+    print(mars_weather)
+    
+    # Close the browser after scraping
+    browser.quit()
+    
+    return mars_weather
+    
+    # -------------------------------------------------------------------------------
+       
+
+def scrape_info():
+    
+    browser = init_browser()
+    
+    # -------------------------------------------------------------------------------
+
+    # URL of NASA MARS news site
+
+    news_url = 'https://mars.nasa.gov/news/'
+
+    browser.visit(news_url)
+    
+    time.sleep(3)
+
+    # Create BeautifulSoup object; parse with 'lxml'
+
+    news_soup = BeautifulSoup(browser.html, 'lxml')
+
+    # Retrieve the title of first news article
+
+    news_title = news_soup.find_all('div', class_='content_title')[1].text
+
+    # Retrieve the paragraph under recent article
+ 
+    news_paragraph = news_soup.find('div', class_='article_teaser_body').text
+
+    print(news_title)
+    print(news_paragraph)
+          
+
+    # -------------------------------------------------------------------------------
+    
+
+    # URL of JPL Mars Space Images
+    
+    spaceimage_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+
+    browser.visit(spaceimage_url)
+    time.sleep(3)
+
+    browser.click_link_by_partial_text('FULL IMAGE')
+    browser.click_link_by_partial_text('more info')
+
+    image_html = browser.html
+
+    image_soup = BeautifulSoup(image_html, 'lxml')
+
+    img_result = image_soup.find('figure', class_ = 'lede')
+
+    image_url = img_result.a['href']
+
+    featured_image_url = 'https://www.jpl.nasa.gov' + image_url
+    
+    print(featured_image_url)
+
+
+    # -------------------------------------------------------------------------------
+    '''
+   
+    # Url of Mars Weather twitter account
+    weather_url = 'https://twitter.com/marswxreport?lang=en'
+
+    browser.visit(weather_url)
+    time.sleep(5)
+    
+    weather_html = browser.html
+
+    weather_soup = BeautifulSoup(weather_html, 'lxml')
+
+    pattern = re.compile(r'InSight sol')
+
+    mars_weather = weather_soup.find('span', text = pattern).text
+
+    print(mars_weather)
+
+    '''
+    # -------------------------------------------------------------------------------
+
+    
+    # URL of Mars Facts webpage
+    facts_url = 'https://space-facts.com/mars/'
+
+    browser.visit(facts_url)
+    time.sleep(3)
+    
+    facts_html = browser.html
+    
+    facts_soup = BeautifulSoup(facts_html, 'lxml')
+
+    mars_facts_table = facts_soup.find('table', id = "tablepress-p-mars")
+
+    mars_facts_html = mars_facts_table.prettify()
+
+    print(mars_facts_html)
+
+
+    # -------------------------------------------------------------------------------
+    
+    
+    # List of Mars hemispheres
+    hemispheres_list = ['Cerberus Hemisphere',
+                        'Schiaparelli Hemisphere',
+                        'Syrtis Major Hemisphere',
+                        'Valles Marineris Hemisphere'
+                       ]
+    
+    # Create a list to add the dictionaries with the hemisphere title and the image url string.
+    hemisphere_image_urls = []
+
+    
+    # URL of page to be scraped
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    for item in hemispheres_list:
+    
+        browser.visit(hemisphere_url)
+    
+        browser.click_link_by_partial_text(item)
+    
+        hemisphere_soup = BeautifulSoup(browser.html,'lxml')
+    
+        title = hemisphere_soup.find('h2', class_ = 'title').text
+        img_url = hemisphere_soup.find("div",class_ ="wide-image-wrapper").find('a')['href']
+    
+        hemisphere_image_urls.append({"Title": title,
+                                      "Image_URL": img_url
+                                     })
+        
+    print(hemisphere_image_urls)
+                 
+    
+    mars_info = {"news_title" : news_title,
+                 "news_paragraph": news_paragraph,
+                 "featured_image_url" : featured_image_url,
+                 "mars_weather" : mars_weather(),
+                 "mars_facts_html" : mars_facts_html,
+                 "hemisphere_image_urls" : hemisphere_image_urls
+                }
+    
+    # Close the browser after scraping
+    browser.quit()
+    
+    return mars_info
+    
+    
+scrape_info()
